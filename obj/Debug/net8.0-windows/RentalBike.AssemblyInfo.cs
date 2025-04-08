5081876a380fa2c0f74fcba498465fd5a71a9170
Boy Simbolon <2281040@unai.edu>
2025-04-09 02:37:25 +0700

    buatkan vuew

--- obj/Debug/net8.0-windows/RentalBike.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RentalBike.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentalBike")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9bb9875f153d32ef0af9532f24adba119baf43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0c9420bb989fa58a00389f88e8d71cd511f5a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentalBike")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentalBike")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
